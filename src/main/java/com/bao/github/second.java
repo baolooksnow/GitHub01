433445be8dd378366715eda86b727fd4555b8369
baogod_glb <baogod_glb@bao.com>
2018-06-16 15:54:17 +0800

    second commit

---
+++ src/main/java/com/bao/github/second.java
@@ -0,0 +1,12 @@
+package com.bao.github;
+
+public class second {
+	public static void main(String[] args) {
+		System.out.println("hello my dear world");
+		
+		System.out.println(" hot_fix  model !");
+		
+		System.out.println("sec_second fix");
+		
+	}
+}
